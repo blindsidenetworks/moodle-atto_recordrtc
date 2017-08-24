864fdcf2c4f6172a19c81456c5090acb0c339f8f
Jacob Prud'homme <jacob.prudhomme@blindsidenetworks.com>
2017-08-24 13:11:23 -0400

    Removed old FontAwesome icons

--- lib.php
+++ lib.php
@@ -38,7 +38,6 @@ function atto_recordrtc_params_for_js($elementid, $options, $fpoptions) {
 
     $moodleversion = intval($CFG->version, 10);
     $moodle32 = 2016120500;
-    $moodle33 = 2017051500;
 
     $context = $options['context'];
     if (!$context) {
@@ -52,10 +51,6 @@ function atto_recordrtc_params_for_js($elementid, $options, $fpoptions) {
     $audiortcicon = 'ed/audiortc';
     $videortcicon = 'ed/videortc';
     $maxrecsize = ini_get('upload_max_filesize');
-    if ($moodleversion >= $moodle33) {
-        $audiortcicon = 'i/audiortc';
-        $videortcicon = 'i/videortc';
-    }
     $params = array('contextid' => $context->id,
                     'sesskey' => $sesskey,
                     'allowedtypes' => $allowedtypes,
@@ -118,14 +113,4 @@ function atto_recordrtc_strings_for_js() {
                );
 
     $PAGE->requires->strings_for_js($strings, 'atto_recordrtc');
-}
-
-/**
- * Map icons for font-awesome themes.
- */
-function atto_recordrtc_get_fontawesome_icon_map() {
-    return [
-        'atto_recordrtc:i/audiortc' => 'fa-file-audio-o',
-        'atto_recordrtc:i/videortc' => 'fa-file-video-o'
-    ];
 }
